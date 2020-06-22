3a4b3e67d766a766b909aaf6ebe76b069f2fba6b
Will Kennedy <willmlb@sbcglobal.net>
2020-06-22 19:30:55 -0400

    bugfix

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
     std::string mode;
     file >> mode;
     std::smatch match;
-    while (mode.size() > 0 && std::regex_match(mode, match, regex))
+    while (!file.eof() && std::regex_match(mode, match, regex))
     {
         printf("Added mode %s\n", match[1]);
         d.addMode(match[1],
